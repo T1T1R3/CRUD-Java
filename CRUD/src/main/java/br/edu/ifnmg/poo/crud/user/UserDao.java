83f0e102f5741e0aa12cea2acec658b6fd316927
AnaLuizanc <analuizanc13@gmail.com>
2024-09-08 20:39:38 -0300

    adds generic methods

--- CRUD/src/main/java/br/edu/ifnmg/poo/crud/user/UserDao.java
+++ CRUD/src/main/java/br/edu/ifnmg/poo/crud/user/UserDao.java
@@ -22,7 +22,30 @@ public class UserDao extends Dao<User>{
         return "update " + TABLE + " email = ?, password = ?, lastAccess = ?, active = ? where id = ?";
     }
 
-    
+    @Override
+    public String getFindByIdStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getFindAllStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getDeleteStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void composeSaveOrUpdateStatement(PreparedStatement pstmt, User e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public User extractObject(ResultSet rs) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
     
     
