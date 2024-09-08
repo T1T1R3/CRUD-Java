4abe3325766850f3ae05492bf9cda600c8782105
AnaLuizanc <analuizanc13@gmail.com>
2024-09-08 16:50:34 -0300

    adds DbConnection's attributes

--- CRUD/src/main/java/br/edu/ifnmg/poo/crud/user/DbConnection.java
+++ CRUD/src/main/java/br/edu/ifnmg/poo/crud/user/DbConnection.java
@@ -1,9 +1,16 @@
 package br.edu.ifnmg.poo.crud.user;
 
+import java.sql.Connection;
+
 /**
  *
  * @author ana
  */
 public class DbConnection {
-    
+
+    private static Connection connection;
+    public static final String URL;
+    private static final String USER;
+    private static final String PASSWORD;
+
 }
