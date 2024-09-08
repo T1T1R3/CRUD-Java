81b5260a2b643a4dff1a8cd4cd705ee9a66bf2b3
AnaLuizanc <analuizanc13@gmail.com>
2024-09-08 20:36:05 -0300

    creates UserDao class

---
+++ CRUD/src/main/java/br/edu/ifnmg/poo/crud/user/UserDao.java
@@ -0,0 +1,18 @@
+
+package br.edu.ifnmg.poo.crud.user;
+
+import br.edu.ifnmg.poo.crud.repository.Dao;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author ana
+ */
+public class UserDao extends Dao<User>{
+    public static final String TABLE = "user";
+
+    
+    
+    
+}
